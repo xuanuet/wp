fb286c8068b5da08234235edea24ccb207417f9d
xuanuet <xuanlt@foobla.com>
2019-03-15 00:51:25 +0700

    style page 2,3

--- wp-content/themes/twentynineteen/template-parts/header/site-branding.php
+++ wp-content/themes/twentynineteen/template-parts/header/site-branding.php
@@ -10,25 +10,18 @@
 <div class="header__inner">
 
 	<?php if ( has_custom_logo() ) : ?>
-		<div class="nav__logo"><?php the_custom_logo(); ?></div>
+		<div class="nav__logo"><h1><?php the_custom_logo(); ?></h1></div>
 	<?php endif; ?>
 	<?php $blog_info = get_bloginfo( 'name' ); ?>
-	<?php if ( ! empty( $blog_info ) ) : ?>
-		<?php if ( is_front_page() && is_home() ) : ?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		<?php else : ?>
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-		<?php endif; ?>
-	<?php endif; ?>
+<!--	--><?php //if ( ! empty( $blog_info ) ) : ?>
+<!--		--><?php //if ( is_front_page() && is_home() ) : ?>
+<!--			<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--		--><?php //else : ?>
+<!--			<p class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></p>-->
+<!--		--><?php //endif; ?>
+<!--	--><?php //endif; ?>
 
-	<?php
-	$description = get_bloginfo( 'description', 'display' );
-	if ( $description || is_customize_preview() ) :
-		?>
-			<p class="site-description">
-				<?php echo $description; ?>
-			</p>
-	<?php endif; ?>
+
 	<div class="gnav">
 		<nav class="nav">
 			<ul>
